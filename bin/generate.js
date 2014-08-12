81cfec2000ddabd65e3b3544cf7876a66d061639
Tony Stuck <tony@cloudflare.com>
2014-08-11 17:35:52 -0700

    Refactored bin file into smaller components that can be used if rolling your own generator

--- bin/generate.js
+++ bin/generate.js
@@ -7,18 +7,11 @@ var colors = require('colors');
 var domain = require('domain').create();
 var Promise = require('bluebird');
 var Generator = require('../index');
-var glob = require('glob');
-var globOptions = {strict: true};
 var _ = require('lodash');
-var Handlebars = require('handlebars');
-var fs = require('fs');
+
 // !Better way to get this?
 var mainConfig = require('../../../package')['json-schema-docs'];
-var handleErr = function (where) {
-	return function (err) {
-		console.error(GEN_PREX+'Error '.red+where+': '+err);
-	}
-};
+var generate = require('../lib/helpers/generate-flow');
 
 domain.on('error', function (err) {
 	console.error(GEN_PREX.red+err);
@@ -27,113 +20,24 @@ domain.on('error', function (err) {
 domain.run(function(){
 	// Allow multiple documentation packages to be defined.
 	// We'll merge in the defaults of the main config for each.
-	var packages = mainConfig.packages || {default: mainConfig},
-		promiseChain = Promise.resolve();
-
+	var packages = mainConfig.packages || {default: mainConfig};
 
 	Promise.all(_.map(packages, function (config, key) {
 		return new Promise(function (resolve, reject) {
 			// Merge the defaults from the main config, less the packages.
 			config = _.defaults(config, _.omit(mainConfig, ['packages']));
+
 			var generator = new Generator(config, runtimeOpts);
+			var processOptions = {
+				id : key,
+				globOptions : {strict: true}
+			};
 
-			var resolveFiles = new Promise(function (resolve, reject) {
-				var schemaConfig = {
-					globs : [].concat(config.schemas),
-					paths : [],
-					done : false
-				};
-				var templateConfig = {
-					globs : [].concat(config.theme ? './node_modules/json-schema-docs-generator/themes/'+config.theme+'/*' : config.templates),
-					paths : [],
-					done : false
-				};
-				var get = function (item) {
-					// Get all the files
-					item.globs.forEach(function (g, i) {
-						glob(g, globOptions, function (err, files) {
-							if (err) return reject(err);
-							// Merge in all the files
-							item.paths = item.paths.concat(files);
-							// End of this config
-							if (i === item.globs.length-1) {
-								item.done = true;
-								// When we've reached the last, move on
-								if (schemaConfig.done && templateConfig.done) {
-									resolve({
-										schemas: schemaConfig.paths,
-										templates: templateConfig.paths
-									});
-								}
-							}
-						});
-					});
-				};
+			return generate(generator, _.extend(processOptions, config), runtimeOpts).then(resolve, reject);
 
-				// Resolve globs
-				get(schemaConfig);
-				get(templateConfig);
-			});
-
-			// Build/generate
-			resolveFiles.then(function (opts) {
-				// Set the expanded schemas and templates config
-				generator.schemas = opts.schemas;
-				generator.templates = opts.templates;
-
-				// Info
-				if (runtimeOpts.verbose) {
-					console.log(GEN_PREX+'Resolving '+generator.schemas.length+' schemas');
-					console.log(GEN_PREX+'Ignoring '+generator.dontParse.length+' files');
-					console.log(GEN_PREX+'Compiling '+generator.templates.length+' templates');
-					console.log(GEN_PREX+'Skipping docs for '+ generator.noDocs.length+ ' schemas');
-					console.log(key+' ---------------------------------');
-				}
-
-				// Deal with templates
-				var templates = generator
-					.parseTemplates()
-					.bind(generator)
-					.then(_.partial(generator.compileTemplates, Handlebars.compile))
-					.then(function (templates) {
-						// Register each template as a partial for Handlebars
-						return _.each(templates, function (acc, compiledSource, name) {
-							Handlebars.registerPartial(name, compiledSource);
-						});
-					}, handleErr('Compiling Templates'));
-
-				// Deal with schemas
-				var schemas = generator
-					.parseSchemas()
-					.bind(generator)
-					.then(generator.resolveSchemas, handleErr('Resolving schemas'));
-
-				// Once both templates and schemas are ready, build the page(s)
-				return Promise.props({templates : templates, schemas : schemas})
-					.bind(generator)
-					.then(function (result) {
-						// Make the page(s) and write pages to disk
-						_.each(generator.makeHTML(result.templates, result.schemas), function (contents, fileName){
-							var folder = (config.destination || 'dist');
-							var path = [folder, '/', fileName, '.html'].join('');
-
-							// Check if the location exists before
-							// trying to save.
-							fs.exists(folder, function (exists) {
-								if (exists) {
-									fs.writeFile(path, contents, function (err) {
-										if (err) { throw new Error(err); }
-										console.log(GEN_PREX+'Wrote file: '+path.green);
-										resolve();
-									});
-								} else {
-									reject(folder+' does not exist. Sorry, I\'m not creating it for you yet.');
-								}
-							});
-						});
-					}, handleErr('Resolving schemas/templates'));
-				});
-		}, handleErr('Fetching schema and templates files'));
+		}).catch(function (err) {
+			console.error(GEN_PREX+'Error '.red+where+': '+err);
+		});
 	})).then(function () {
 		console.log('--------------------------------------');
 		console.log(GEN_PREX+'Build took '+ process.uptime() + ' seconds');
